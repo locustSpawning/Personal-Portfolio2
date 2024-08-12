b3e311dda50f072a0ec8236cb39e8c2933a48ce0
chels <chelsleonhardt@gmail.com>
2024-08-12 14:46:05 -0400

    Animation stylized

---
+++ script.js
@@ -0,0 +1,15 @@
+const textAnimationGoesHere = document.getElementById('auto-type');
+
+var typed = new Typed(textAnimationGoesHere, {
+    strings: [
+        'design.',
+        'typography.',
+        'writing.',
+        'illustration.',
+        'books.',
+        'plants.',
+    ],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true,
+});
